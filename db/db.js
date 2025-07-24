04b24b591b18a3e7fe98e4f8188c5b685291f530
Ayush Pradhan <AAYUSHPRADHAN996@outlook.com>
2025-07-24 09:53:59 +0545

    connectingserver to database

--- db/db.js
+++ db/db.js
@@ -2,11 +2,11 @@ import mongoose from "mongoose";
 import {NODE_ENV} from "../config/env.js";
 
 const DB_URL="mongodb+srv://aayushjs:aayushjs12345@cluster0.uuqee4l.mongodb.net/"
-const DB_NAME="subscriptiontracker";
+// const DB_NAME="subscriptiontracker";
 
 const dbConnection = async () => {
     try {
-        const connectionString = await mongoose.connect(`${DB_URL}/${DB_NAME}`);
+        const connectionString = await mongoose.connect(`${DB_URL}`);
         return console.log(`MONGODB IS SUCCESSFULLY CONNECTED TO ${NODE_ENV} MODE`);
     } catch (error) {
         console.log("MONGODB CONNECTION IS FAILED", error)
