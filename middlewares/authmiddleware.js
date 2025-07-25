f9fa18d0e33151a26e7bac52c9eeb4d5d6241f5a
Ayush Pradhan <AAYUSHPRADHAN996@outlook.com>
2025-07-25 10:04:16 +0545

    authorization logic: checking actual user or not

--- middlewares/authmiddleware.js
+++ middlewares/authmiddleware.js
@@ -6,14 +6,15 @@ export const authMiddleware = async (req, res, next) => {
     try {
         let token;
 
-        if (req.headers.authorization && req.headers.authorization.startswith('Bearer')) {
+        if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
             token = req.headers.authorization.split(' ')[1];
         }
+        console.log(token);
 
         if (!token) return next(401, "UNAUTHORIZED REQUEST: NO TOKEN PROVIDED");
-
          // eslint-disable-next-line no-undef
         const decode = jwt.verify(token, process.env.JWT_SECRET);
+        console.log(decode)
 
         const user = await User.findById(decode.userId);
         if (!user) return res
@@ -27,6 +28,7 @@ export const authMiddleware = async (req, res, next) => {
     } catch (error) {
         res
             .status(500)
-            .json({ success: false, message: error.message });
+            .json({ success: false,
+                 message: error.message });
     }
 }
